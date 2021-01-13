6a320ab78238f53bd395fe6d2b58425ba4e0f9d1
tetsu <cda07c5117@gmail.com>
2021-01-14 02:14:39 +0900

    ページ認証機能実装
    ゲストが見れない予定のページにアクセスするとログイン画面にリダイレクト

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,31 @@ use App\Http\Controllers\LikeController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::group(['middleware' => 'auth'], function() {
 
-Auth::routes();
+  Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+  Route::get('/timeline/post/create', [App\Http\Controllers\PostController::class, 'showCreateForm'])->name('create');
+  Route::post('/timeline/post/create', [App\Http\Controllers\PostController::class, 'create']);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+  Route::get('/timeline/post/{post_id}/edit', [App\Http\Controllers\PostController::class, 'showEditForm'])->name('edit');
+  Route::post('/timeline/post/{post_id}/edit', [App\Http\Controllers\PostController::class, 'edit']);
+
+  Route::post('/timeline/post/{post_id}/delete', [App\Http\Controllers\PostController::class, 'delete'])->name('delete');
+
+  Route::post('/timeline/post/{post_id}/comment/create', [App\Http\Controllers\CommentController::class, 'create'])->name('commentCreate');
+
+  Route::post('/timeline/post/{post_id}/comment/delete', [App\Http\Controllers\CommentController::class, 'delete'])->name('commentDelete');
+
+  Route::post('/timeline/like/create', [App\Http\Controllers\LikeController::class, 'create'])->name('likeCreate');
+
+  Route::post('/timeline/like/delete', [App\Http\Controllers\LikeController::class, 'delete'])->name('likeDelete');
+
+});
 
 
 Route::get('/timeline', [App\Http\Controllers\PostController::class, 'timeline'])->name('timeline');
 
-Route::get('/timeline/post/create', [App\Http\Controllers\PostController::class, 'showCreateForm'])->name('create');
-Route::post('/timeline/post/create', [App\Http\Controllers\PostController::class, 'create']);
-
 Route::get('/timeline/post/{post_id}', [App\Http\Controllers\PostController::class, 'show'])->name('show');
 
-Route::get('/timeline/post/{post_id}/edit', [App\Http\Controllers\PostController::class, 'showEditForm'])->name('edit');
-Route::post('/timeline/post/{post_id}/edit', [App\Http\Controllers\PostController::class, 'edit']);
-
-Route::post('/timeline/post/{post_id}/delete', [App\Http\Controllers\PostController::class, 'delete'])->name('delete');
-
-Route::post('/timeline/post/{post_id}/comment/create', [App\Http\Controllers\CommentController::class, 'create'])->name('commentCreate');
-
-Route::post('/timeline/post/{post_id}/comment/delete', [App\Http\Controllers\CommentController::class, 'delete'])->name('commentDelete');
-
-Route::post('/timeline/like/create', [App\Http\Controllers\LikeController::class, 'create'])->name('likeCreate');
-
-Route::post('/timeline/like/delete', [App\Http\Controllers\LikeController::class, 'delete'])->name('likeDelete');
+Auth::routes();
