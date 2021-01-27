9def3aabc0cb0e4ecb5aa108858917a1e35e8f0b
tetsu <cda07c5117@gmail.com>
2021-01-28 02:24:52 +0900

    画像投稿表示先変更

--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -78,7 +78,7 @@
     @if ($post->file_path)
       <div class="card rounded-lg shadow-sm mb-3 bg-white reference-image">
         <p class="card-header">添付画像</p>
-        <img src="{{ Storage::url($post->file_path) }}" class="card-body p-1 m-1 border border-primary rounded expansion" style="width:100px; height:100px">
+        <img src="{{ Storage::url($post->file_name) }}" class="card-body p-1 m-1 border border-primary rounded expansion" style="width:100px; height:100px">
       </div>
     @endif
     <div class="rounded-lg shadow-sm p-3 mb-3 bg-white">
