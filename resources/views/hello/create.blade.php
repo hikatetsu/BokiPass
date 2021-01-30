492be881120f59da98ee9f0b99709b95b88cc82d
tetsu <cda07c5117@gmail.com>
2021-01-31 01:50:06 +0900

    html修正

--- resources/views/hello/create.blade.php
+++ resources/views/hello/create.blade.php
@@ -1,75 +1,73 @@
 @extends('layout')
 
 @section('content')
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="container my-3">
-        <h1 class="h4">合格体験記 投稿フォーム</h1>
-        <!-- 入力エラーがある場合は表示 -->
-        @if($errors->any())
-          <ul class="list-unstyled">
-            @foreach($errors->all() as $message)
-              <li class="alert alert-danger">{{ $message }}</li>
-            @endforeach
-          </ul>
-        @endif
+  <div class="col-md-8 container">
+    <div class=" my-3">
+      <h1 class="h4">合格体験記 投稿フォーム</h1>
+      <!-- 入力エラーがある場合は表示 -->
+      @if($errors->any())
+        <ul class="list-unstyled">
+          @foreach($errors->all() as $message)
+            <li class="alert alert-danger">{{ $message }}</li>
+          @endforeach
+        </ul>
+      @endif
+    </div>
+    <!-- 合格体験記入力フォーム -->
+    <form action="{{route('create')}}" method="post" class=" mb-4" enctype="multipart/form-data">
+      @csrf
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="pass_class" class="font-weight-bold">何級に合格しましたか？</label><br>
+        <input type="radio" name="pass_class" id="pass_class" value="１" {{old('pass_class') == "１" ? 'checked' : ''}} checked>１級
+        <input type="radio" name="pass_class" id="pass_class" value="２" {{old('pass_class') == "２" ? 'checked' : ''}} class="ml-2">２級
+        <input type="radio" name="pass_class" id="pass_class" value="３" {{old('pass_class') == "３" ? 'checked' : ''}} class="ml-2">３級
+        <input type="radio" name="pass_class" id="pass_class" value="初" {{old('pass_class') == "初" ? 'checked' : ''}} class="ml-2">初級
       </div>
-      <!-- 合格体験記入力フォーム -->
-      <form action="{{route('create')}}" method="post" class="container mb-4" enctype="multipart/form-data">
-        @csrf
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="pass_class" class="font-weight-bold">何級に合格しましたか？</label><br>
-          <input type="radio" name="pass_class" id="pass_class" value="１" {{old('pass_class') == "１" ? 'checked' : ''}} checked>１級
-          <input type="radio" name="pass_class" id="pass_class" value="２" {{old('pass_class') == "２" ? 'checked' : ''}} class="ml-2">２級
-          <input type="radio" name="pass_class" id="pass_class" value="３" {{old('pass_class') == "３" ? 'checked' : ''}} class="ml-2">３級
-          <input type="radio" name="pass_class" id="pass_class" value="初" {{old('pass_class') == "初" ? 'checked' : ''}} class="ml-2">初級
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="test_style" class="font-weight-bold">どの試験方式でしたか？</label><br>
-          <input type="radio" name="test_style" id="test_style" value="筆記試験" {{old('test_style') == "筆記試験" ? 'checked' : ''}} checked>筆記試験 
-          <input type="radio" name="test_style" id="test_style" value="ネット試験" {{old('test_style') == "ネット試験" ? 'checked' : ''}} class="ml-2">ネット試験
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="nunber_times" class="font-weight-bold">受験回数は何回ですか？</label><br>
-          <input type="radio" name="nunber_times" id="nunber_times" value="１回" {{old('nunber_times') == "１回" ? 'checked' : ''}} checked>１回
-          <input type="radio" name="nunber_times" id="nunber_times" value="２回" {{old('nunber_times') == "２回" ? 'checked' : ''}} class="ml-2">２回
-          <input type="radio" name="nunber_times" id="nunber_times" value="３回" {{old('nunber_times') == "３回" ? 'checked' : ''}} class="ml-2">３回
-          <input type="radio" name="nunber_times" id="nunber_times" value="４回以上" {{old('nunber_times') == "４回以上" ? 'checked' : ''}} class="ml-2">４回以上
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="pass_date" class="w-100 font-weight-bold">いつ合格しましたか？</label><br>
-          <input type="month" name="pass_date" id="pass_date" value="{{old('pass_date',date("Y-m"))}}" class=" p-1 input-pass-date" placeholder="記載例：2021-01(半角)">
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="study_period" class="w-100 font-weight-bold">勉強期間(時間)はどれくらいでしたか？</label><br>
-          <textarea name="study_period" id="study_period" rows="5" placeholder="記載例：何ヶ月間や合計何時間など。平日は何時間で休日は何時間など。" class="w-100 p-1">{{old('study_period')}}</textarea>
-          <small class="form-text text-muted">191文字まで</small>
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="study_method" class="w-100 font-weight-bold">どのような勉強法でしたか？</label><br>
-          <textarea name="study_method" id="study_method" rows="5" placeholder="記載例：独学or通信講座or通学？通信講座や通学ならスクール名など。その他具体的な勉強法。" class="w-100 p-1">{{old('study_method')}}</textarea>
-          <small class="form-text text-muted">191文字まで</small>
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="books_used" class="w-100 font-weight-bold">使用した教材は何ですか？</label><br>
-          <textarea name="books_used" id="books_used" rows="5" placeholder="記載例：教材やWebサービスの名前。それらの特徴など。" class="w-100 p-1">{{old('books_used')}}</textarea>
-          <small class="form-text text-muted">191文字まで</small>
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="advice" class="w-100 font-weight-bold">合格の秘訣や受験生へアドバイスをお願いします。</label><br>
-          <textarea name="advice" id="advice" rows="5" placeholder="記載例：おすすめの勉強法や受験上の注意点など。" class="w-100 p-1">{{old('advice')}}</textarea>
-          <small class="form-text text-muted">191文字まで</small>
-        </div>
-        <div class="container form-group rounded-lg shadow-sm p-3 bg-white">
-          <label for="image" class="w-100 font-weight-bold">画像を添付（任意）</label><br>
-          <input type="file" name="image" accept="image/png, image/jpeg" id="image">
-          <small class="form-text text-muted">著作権違反にご注意ください。</small>
-        </div>
-        <button type="submit" onClick="return double()" class="btn btn-primary">　送　信　</button>
-      </form>
-      <div class="container pb-3">
-        <a href="{{route('timeline')}}" class="btn btn-outline-dark">キャンセル</a>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="test_style" class="font-weight-bold">どの試験方式でしたか？</label><br>
+        <input type="radio" name="test_style" id="test_style" value="筆記試験" {{old('test_style') == "筆記試験" ? 'checked' : ''}} checked>筆記試験 
+        <input type="radio" name="test_style" id="test_style" value="ネット試験" {{old('test_style') == "ネット試験" ? 'checked' : ''}} class="ml-2">ネット試験
       </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="nunber_times" class="font-weight-bold">受験回数は何回ですか？</label><br>
+        <input type="radio" name="nunber_times" id="nunber_times" value="１回" {{old('nunber_times') == "１回" ? 'checked' : ''}} checked>１回
+        <input type="radio" name="nunber_times" id="nunber_times" value="２回" {{old('nunber_times') == "２回" ? 'checked' : ''}} class="ml-2">２回
+        <input type="radio" name="nunber_times" id="nunber_times" value="３回" {{old('nunber_times') == "３回" ? 'checked' : ''}} class="ml-2">３回
+        <input type="radio" name="nunber_times" id="nunber_times" value="４回以上" {{old('nunber_times') == "４回以上" ? 'checked' : ''}} class="ml-2">４回以上
+      </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="pass_date" class="w-100 font-weight-bold">いつ合格しましたか？</label><br>
+        <input type="month" name="pass_date" id="pass_date" value="{{old('pass_date',date("Y-m"))}}" class=" p-1 input-pass-date" placeholder="記載例：2021-01(半角)">
+      </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="study_period" class="w-100 font-weight-bold">勉強期間(時間)はどれくらいでしたか？</label><br>
+        <textarea name="study_period" id="study_period" rows="5" placeholder="記載例：何ヶ月間や合計何時間など。平日は何時間で休日は何時間など。" class="w-100 p-1">{{old('study_period')}}</textarea>
+        <small class="form-text text-muted">191文字まで</small>
+      </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="study_method" class="w-100 font-weight-bold">どのような勉強法でしたか？</label><br>
+        <textarea name="study_method" id="study_method" rows="5" placeholder="記載例：独学or通信講座or通学？通信講座や通学ならスクール名など。その他具体的な勉強法。" class="w-100 p-1">{{old('study_method')}}</textarea>
+        <small class="form-text text-muted">191文字まで</small>
+      </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="books_used" class="w-100 font-weight-bold">使用した教材は何ですか？</label><br>
+        <textarea name="books_used" id="books_used" rows="5" placeholder="記載例：教材やWebサービスの名前。それらの特徴など。" class="w-100 p-1">{{old('books_used')}}</textarea>
+        <small class="form-text text-muted">191文字まで</small>
+      </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="advice" class="w-100 font-weight-bold">合格の秘訣や受験生へアドバイスをお願いします。</label><br>
+        <textarea name="advice" id="advice" rows="5" placeholder="記載例：おすすめの勉強法や受験上の注意点など。" class="w-100 p-1">{{old('advice')}}</textarea>
+        <small class="form-text text-muted">191文字まで</small>
+      </div>
+      <div class=" form-group rounded-lg shadow-sm p-3 bg-white">
+        <label for="image" class="w-100 font-weight-bold">画像を添付（任意）</label><br>
+        <input type="file" name="image" accept="image/png, image/jpeg" id="image">
+        <small class="form-text text-muted">著作権違反にご注意ください。</small>
+      </div>
+      <button type="submit" onClick="return double()" class="btn btn-primary">　送　信　</button>
+    </form>
+    <div class=" pb-3">
+      <a href="{{route('timeline')}}" class="btn btn-outline-dark">キャンセル</a>
     </div>
   </div>
 @endsection
--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -1,137 +1,135 @@
 @extends('layout')
 
 @section('content')
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <!-- ゲストかユーザーかをチェックし、合格体験記を投稿した本人(ユーザー)のみ編集・削除が可能 -->
-      <div class="container text-right mt-3">
-        @guest
-          @else
-            @if(auth()->user()->id == $post->user_id)
-              <form action="{{route('edit', ['post_id' => $post->id])}}" method="get" class="d-inline">
-                @csrf
-                <button type="submit" onClick="return double()" class="btn btn-outline-dark btn-sm">編集</button>
-              </form>
-              <form action="{{route('delete', ['post_id' => $post->id])}}" method="post" class="d-inline">
-                @csrf
-                <button type="submit" id="deletebtn" onClick="return check()" class="btn btn-outline-danger btn-sm">削除</button>
-              </form>
-            @endif
-        @endguest
-      </div>
-      <div class="container mt-2">
-        <!-- メッセージがある場合は表示 -->
-        @if (session('status'))
-          <p class="alert alert-info">{{ session('status') }}</p> 
-        @endif
-        <!-- 入力エラーがある場合は表示 -->
-        @if($errors->any())
-          <div>
-            <ul class="list-unstyled">
-              @foreach($errors->all() as $message)
-                <li class="alert alert-danger">{{ $message }}</li>
-              @endforeach
-            </ul>
-          </div>
-        @endif
-      </div>
-      <!-- 合格体験記詳細を表示 -->
-      <div class="container">
+  <div class="col-md-8 container">
+    <!-- ゲストかユーザーかをチェックし、合格体験記を投稿した本人(ユーザー)のみ編集・削除が可能 -->
+    <div class=" text-right mt-3">
+      @guest
+        @else
+          @if(auth()->user()->id == $post->user_id)
+            <form action="{{route('edit', ['post_id' => $post->id])}}" method="get" class="d-inline">
+              @csrf
+              <button type="submit" onClick="return double()" class="btn btn-outline-dark btn-sm">編集</button>
+            </form>
+            <form action="{{route('delete', ['post_id' => $post->id])}}" method="post" class="d-inline">
+              @csrf
+              <button type="submit" id="deletebtn" onClick="return check()" class="btn btn-outline-danger btn-sm">削除</button>
+            </form>
+          @endif
+      @endguest
+    </div>
+    <div class=" mt-2">
+      <!-- メッセージがある場合は表示 -->
+      @if (session('status'))
+        <p class="alert alert-info">{{ session('status') }}</p> 
+      @endif
+      <!-- 入力エラーがある場合は表示 -->
+      @if($errors->any())
         <div>
-          <h1 class="h4">合格体験記 No.{{$post->id}}</h1>
-          <h2 class="h3 font-weight-bold bg-{{$post->style_pass_class}} text-white d-inline p-1">日商簿記検定{{$post->pass_class}}級</h2>
-          <div class="my-2">
-          <small class="form-text text-muted d-inline mr-2">投稿日{{$post->created_at->format('Y.m.d')}}</small>
-          <small class="form-text text-muted d-inline">更新日{{$post->updated_at->format('Y.m.d')}}</small>
-          </div>
+          <ul class="list-unstyled">
+            @foreach($errors->all() as $message)
+              <li class="alert alert-danger">{{ $message }}</li>
+            @endforeach
+          </ul>
         </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">投稿者</p>
-          <p class="card-body mb-0">ID{{$post->user_id}} {{$post->user_name}}さん</p>
+      @endif
+    </div>
+    <!-- 合格体験記詳細を表示 -->
+    <div class="">
+      <div>
+        <h1 class="h4">合格体験記 No.{{$post->id}}</h1>
+        <h2 class="h3 font-weight-bold bg-{{$post->style_pass_class}} text-white d-inline p-1">日商簿記検定{{$post->pass_class}}級</h2>
+        <div class="my-2">
+        <small class="form-text text-muted d-inline mr-2">投稿日{{$post->created_at->format('Y.m.d')}}</small>
+        <small class="form-text text-muted d-inline">更新日{{$post->updated_at->format('Y.m.d')}}</small>
         </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">合格年月</p>
-          <p class="card-body mb-0">{{$post->pass_date}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">投稿者</p>
+        <p class="card-body mb-0">ID{{$post->user_id}} {{$post->user_name}}さん</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">合格年月</p>
+        <p class="card-body mb-0">{{$post->pass_date}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">受験方式</p>
+        <p class="card-body mb-0">{{$post->test_style}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">受験回数</p>
+        <p class="card-body mb-0">{{$post->nunber_times}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">勉強期間（時間）</p>
+        <p class="card-body mb-0">{{$post->study_period}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">勉強法</p>
+        <p class="card-body mb-0">{{$post->study_method}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">使用した教材</p>
+        <p class="card-body mb-0">{{$post->books_used}}</p>
+      </div>
+      <div class="card rounded-lg shadow-sm mb-3 bg-white">
+        <p class="card-header">合格した秘訣や受験生へのアドバイス</p>
+        <p class="card-body mb-0">{{$post->advice}}</p>
+      </div>
+      @if ($post->file_path)
+        <div class="card rounded-lg shadow-sm mb-3 bg-white reference-image">
+          <p class="card-header">添付画像</p>
+          <img src="{{$post->file_path}}" class="card-body p-1 m-1 border border-primary rounded expansion" style="width:100px; height:100px">
         </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">受験方式</p>
-          <p class="card-body mb-0">{{$post->test_style}}</p>
-        </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">受験回数</p>
-          <p class="card-body mb-0">{{$post->nunber_times}}</p>
-        </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">勉強期間（時間）</p>
-          <p class="card-body mb-0">{{$post->study_period}}</p>
-        </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">勉強法</p>
-          <p class="card-body mb-0">{{$post->study_method}}</p>
-        </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">使用した教材</p>
-          <p class="card-body mb-0">{{$post->books_used}}</p>
-        </div>
-        <div class="card rounded-lg shadow-sm mb-3 bg-white">
-          <p class="card-header">合格した秘訣や受験生へのアドバイス</p>
-          <p class="card-body mb-0">{{$post->advice}}</p>
-        </div>
-        @if ($post->file_path)
-          <div class="card rounded-lg shadow-sm mb-3 bg-white reference-image">
-            <p class="card-header">添付画像</p>
-            <img src="{{$post->file_path}}" class="card-body p-1 m-1 border border-primary rounded expansion" style="width:100px; height:100px">
-          </div>
-        @endif
-        <div class="rounded-lg shadow-sm p-3 mb-3 bg-white">
-          <!-- ゲストかユーザーかをチェックし、ユーザーならコメントが可能 -->
-          @guest
-              <div>
-              <p class="">コメント欄</p>
-              <small class="form-text text-muted">ログインするとコメントができます。</small>
-              </div>
-            @else
-              <form action="{{route('commentCreate', ['post_id' => $post->id])}}" method="post">
-                @csrf
-                <div>
-                  <label for="comment" class="w-100">コメント欄</label><br>
-                  <textarea name="body" id="comment" rows="5" placeholder="コメントをどうぞ" class="p-1 w-100  rounded-lg">{{old('body')}}</textarea>
-                  <small class="form-text text-muted">191文字まで</small>
-                </div>
-                <button type="submit" onClick="return double()" class="btn btn-primary my-2">コメントする</button>
-              </form>
-          @endguest
-          <!-- 全てのコメントを表示・ゲストかユーザーかをチェックし、コメントした本人(ユーザー)のみ削除が可能 -->
-          @foreach($comments as $comment)
-            <div class="card m-2">
-              <p class="card-header">ID{{$comment->user_id}} {{$comment->user_name}}さん</p>
-              <div class="card-body">
-                  <p class="card-text">{{$comment->body}}</p>
-                  <small class="form-text text-muted">{{$comment->created_at->format('Y.m.d H:i')}}</small>
-                @guest
-                  @else
-                    @if(auth()->user()->id == $comment->user_id)
-                      <form action="{{route('commentDelete', ['post_id' => $post->id])}}" method="post">
-                        @csrf
-                        <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                        <div class="text-right">
-                          <button type="submit" onClick="return check()" class="btn btn-outline-danger btn-sm">削除</button>
-                        </div>
-                      </form>
-                    @endif
-                @endguest
-              </div>
+      @endif
+      <div class="rounded-lg shadow-sm p-3 mb-3 bg-white">
+        <!-- ゲストかユーザーかをチェックし、ユーザーならコメントが可能 -->
+        @guest
+            <div>
+            <p class="">コメント欄</p>
+            <small class="form-text text-muted">ログインするとコメントができます。</small>
             </div>
-          @endforeach
+          @else
+            <form action="{{route('commentCreate', ['post_id' => $post->id])}}" method="post">
+              @csrf
+              <div>
+                <label for="comment" class="w-100">コメント欄</label><br>
+                <textarea name="body" id="comment" rows="5" placeholder="コメントをどうぞ" class="p-1 w-100  rounded-lg">{{old('body')}}</textarea>
+                <small class="form-text text-muted">191文字まで</small>
+              </div>
+              <button type="submit" onClick="return double()" class="btn btn-primary my-2">コメントする</button>
+            </form>
+        @endguest
+        <!-- 全てのコメントを表示・ゲストかユーザーかをチェックし、コメントした本人(ユーザー)のみ削除が可能 -->
+        @foreach($comments as $comment)
+          <div class="card m-2">
+            <p class="card-header">ID{{$comment->user_id}} {{$comment->user_name}}さん</p>
+            <div class="card-body">
+                <p class="card-text">{{$comment->body}}</p>
+                <small class="form-text text-muted">{{$comment->created_at->format('Y.m.d H:i')}}</small>
+              @guest
+                @else
+                  @if(auth()->user()->id == $comment->user_id)
+                    <form action="{{route('commentDelete', ['post_id' => $post->id])}}" method="post">
+                      @csrf
+                      <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                      <div class="text-right">
+                        <button type="submit" onClick="return check()" class="btn btn-outline-danger btn-sm">削除</button>
+                      </div>
+                    </form>
+                  @endif
+              @endguest
+            </div>
+          </div>
+        @endforeach
+    </div>
+      <!-- ページネーション -->
+      <div class="d-flex justify-content-center">
+          {{ $comments->links('vendor/pagination/pagination_view') }}
       </div>
-        <!-- ページネーション -->
-        <div class="d-flex justify-content-center">
-            {{ $comments->links('vendor/pagination/pagination_view') }}
-        </div>
-      </div>
-      <div class="container">
-        <a href="{{route('timeline')}}" class="btn btn-outline-dark mb-3">TOPへ戻る</a>
-      </div>
+    </div>
+    <div class="">
+      <a href="{{route('timeline')}}" class="btn btn-outline-dark mb-3">TOPへ戻る</a>
     </div>
   </div>
 @endsection
