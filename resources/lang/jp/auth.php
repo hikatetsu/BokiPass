71326f0d2c4c94d95630f2cece677d63617a7c19
tetsu <cda07c5117@gmail.com>
2021-02-08 00:27:03 +0900

    ログインエラーメッセージ日本語化

--- resources/lang/jp/auth.php
+++ resources/lang/jp/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'failed' => 'メールアドレスまたはパスワードに誤りがあります。',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'password' => '入力されたパスワードが正しくありません。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds秒後にもう一度お試しください。',
 
 ];
